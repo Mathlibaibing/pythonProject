0b2365284d15cfec4c9253338e274624eb390261
white <math_libaibing@foxmail.com>
2021-06-13 11:58:18 +0800

    增加solution2.py文件

---
+++ practice/solution2.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        ret = None
+        return ret
+
+if __name__ == '__main__':
+    example = Solution()
+    example.fourSum([],3)
